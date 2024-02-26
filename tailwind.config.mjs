e32294a4e6796ff14157b91722074cf3de3a3ac9
Tomas Maldocena <104042642+tmaldocena@users.noreply.github.com>
2024-02-26 16:25:55 -0300

    feat: Agregado el navbar, con componente de botones y progreso de la seccion here

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from 'tailwindcss/defaultTheme'
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        violet: '#ae81e9',
+        white: '#ffffff',
+        black: '#1E1E1E'
+      }
+    },
+    fontFamily: {
+      serif: ['Sora', defaultTheme.fontFamily.serif]
+    }
   },
   plugins: []
 }
