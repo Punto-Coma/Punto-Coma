f57bad122deffd1e6336069b381263bae9d52b13
Lapsa09 <agustin.dinardo@vicentelopez.gov.ar>
2024-02-27 09:29:31 -0300

    feat: seccion about
    seccion acerca de
    
    section about

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -9,6 +9,17 @@ export default {
         violet: '#ae81e9',
         white: '#ffffff',
         black: '#0e0e0e'
+      },
+      fontSize: {
+        xs: ['12px', '16px'],
+        sm: ['14px', '20px'],
+        base: ['16px', '24px'],
+        lg: ['18px', '28px'],
+        xl: ['20px', '28px'],
+        '2xl': ['24px', '32px'],
+        '3xl': ['30px', '36px'],
+        '4xl': ['36px', '40px'],
+        '5xl': ['48px', '100px']
       }
     },
     fontFamily: {
